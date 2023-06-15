7adb63f3a083af98af8355b031426a8e7679bc9d
brijal.kansara <brijal.kansara@trinarybits.in>
2023-06-15 15:10:07 +0530

    [React form hook]

---
+++ src/components/YouTubeForm.tsx
@@ -0,0 +1,54 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
+export const YouTubeForm = () => {
+  const {register, control, handleSubmit, formState} = useForm<FormValues>();
+
+  // Note things are 
+  // Register is for Managing the data and Validation
+  // Control is for Devtool for us to understand the things
+  // handleSubmit is for the submission of the data
+  // formState is focused to show errors for now
+
+  // For fields we need to create following interface
+  // And do not forget to pass it to useForm hook and data to gather.
+  type FormValues = {
+    username:string,
+  email:string,
+  channel: string
+}
+
+const onSubmit = (data:FormValues) => {
+  console.log(data)
+ }
+
+    return (
+     <>
+       <div>
+        <h1>YouTube Form</h1>
+  
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" {...register('username', {
+            // Here it goes for validations
+            required:"User Name is required."
+          })}/>
+          <p style={{color:'red'}}>{formState.errors.username?.message}</p>
+  
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" {...register('email',{
+            // Here it goes for validations
+            required:"Email Name is required."
+          })}/>
+          // showing erroe here
+          <p style={{color:'red'}}>{formState.errors.email?.message}</p>
+          <label htmlFor="channel">Channel</label>
+          <input type="text" id="channel" {...register('channel')} />
+  
+          <button>Submit</button>
+        </form>
+      </div>
+      <DevTool control={control}/>
+     </>
+    );
+  };
